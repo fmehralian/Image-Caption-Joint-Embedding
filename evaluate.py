d6e61a027edc0c56609aa9172ec7a93a627960ff
Forough <forough@Foroughs-MacBook-Pro.local>
2020-03-01 10:43:25 -0800

    find erroronous cases

--- evaluate.py
+++ evaluate.py
@@ -1,21 +1,22 @@
 import numpy
 import torch
 
-def image_to_text(captions, images, npts=None, verbose=False):
+
+def image_to_text(contents, captions, images, npts=None, verbose=False):
     """
     Images->Text (Image Annotation)
-    Images: (5N, K) matrix of images
-    Captions: (5N, K) matrix of captions
+    Images: (N, K) matrix of images
+    Captions: (N, K) matrix of captions
     """
     if npts == None:
-        npts = images.size()[0] / 5
+        npts = images.size()[0]
         npts = int(npts)
 
     ranks = numpy.zeros(npts)
+    gen = []
     for index in range(npts):
-
         # Get query image
-        im = images[5 * index].unsqueeze(0)
+        im = images[index].unsqueeze(0)
 
         # Compute scores
         d = torch.mm(im, captions.t())
@@ -23,52 +24,57 @@ def image_to_text(captions, images, npts=None, verbose=False):
         inds = inds.data.squeeze(0).cpu().numpy()
 
         # Score
-        rank = 1e20
-        # find the highest ranking
-        for i in range(5*index, 5*index + 5, 1):
-            tmp = numpy.where(inds == i)[0][0]
-            if tmp < rank:
-                rank = tmp
-        ranks[index] = rank
+        ranks[index] = numpy.where(inds == index)[0][0]
+        gen[index] = contents[inds[0]].split("_")[-1]
 
     # Compute metrics
     r1 = 100.0 * len(numpy.where(ranks < 1)[0]) / len(ranks)
     r5 = 100.0 * len(numpy.where(ranks < 5)[0]) / len(ranks)
     r10 = 100.0 * len(numpy.where(ranks < 10)[0]) / len(ranks)
     medr = numpy.floor(numpy.median(ranks)) + 1
+    with open('err_im_to_text', 'w') as f:
+        f.write('>10:' + str([contents[i] for i in numpy.where(ranks > 10)[0]]) + "\n")
+        f.write('>5:' + str([contents[i] for i in numpy.where(ranks > 5)[0]]) + "\n")
+        f.write('>1:' + str([contents[i] for i in numpy.where(ranks > 1)[0]]) + "\n")
+        f.write(gen)
 
     if verbose:
         print("		* Image to text scores: R@1: %.1f, R@5: %.1f, R@10: %.1f, Medr: %.1f" % (r1, r5, r10, medr))
-    return r1+r5+r10, (r1, r5, r10, medr)
+    return r1 + r5 + r10, (r1, r5, r10, medr)
 
-def text_to_image(captions, images, npts=None, verbose=False):
+
+def text_to_image(contents, captions, images, npts=None, verbose=False):
     if npts == None:
-    	npts = images.size()[0] / 5
-    	npts = int(npts)
+        npts = images.size()[0]
+        npts = int(npts)
 
-    ims = torch.cat([images[i].unsqueeze(0) for i in range(0, len(images), 5)])
+    ims = torch.cat([images[i].unsqueeze(0) for i in range(0, len(images), 1)])
 
-    ranks = numpy.zeros(5 * npts)
+    ranks = numpy.zeros(npts)
     for index in range(npts):
 
         # Get query captions
-        queries = captions[5*index : 5*index + 5]
+        queries = captions[index]
+        queries = queries.unsqueeze(0)
 
         # Compute scores
         d = torch.mm(queries, ims.t())
         for i in range(d.size()[0]):
             d_sorted, inds = torch.sort(d[i], descending=True)
             inds = inds.data.squeeze(0).cpu().numpy()
-            ranks[5 * index + i] = numpy.where(inds == index)[0][0]
+            ranks[1 * index + i] = numpy.where(inds == index)[0][0]
 
     # Compute metrics
     r1 = 100.0 * len(numpy.where(ranks < 1)[0]) / len(ranks)
     r5 = 100.0 * len(numpy.where(ranks < 5)[0]) / len(ranks)
     r10 = 100.0 * len(numpy.where(ranks < 10)[0]) / len(ranks)
     medr = numpy.floor(numpy.median(ranks)) + 1
-    
+    with open('err_txt_to_img', 'w') as f:
+        f.write('>10:' + str([contents[i] for i in numpy.where(ranks > 10)[0]]) + "\n")
+        f.write('>5:' + str([contents[i] for i in numpy.where(ranks > 5)[0]]) + "\n")
+        f.write('>1:' + str([contents[i] for i in numpy.where(ranks > 1)[0]]) + "\n")
     if verbose:
         print("		* Text to image scores: R@1: %.1f, R@5: %.1f, R@10: %.1f, Medr: %.1f" % (r1, r5, r10, medr))
-    return r1+r5+r10, (r1, r5, r10, medr)
+    return r1 + r5 + r10, (r1, r5, r10, medr)
 
- 
+
